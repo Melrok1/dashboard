81ee46417f09c51ba49a7d62269bcecc31906cbe
Melrok <martin.blascak86@gmail.com>
2023-11-06 11:10:11 +0100

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <div class="create-note-wrapper">
+
+    </div>
+
+    <div class="max-w-xl mx-auto p-4">
+      <div class="bg-white p-4 rounded-lg shadow-lg">
+        <textarea
+          class="w-full h-20 p-4 text-gray-700 bg-gray-100 rounded-md border-0 focus:ring-2 focus:ring-blue-200 focus:bg-white resize-none"
+          placeholder="Vytvoriť poznámku..." autofocus>
+        </textarea>
+      </div>
+    </div>
+  </main>
+</template>
